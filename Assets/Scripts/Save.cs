46efb80402c7404bc708c74ca7cf909ac8611e24
Artekosh <artekosh1@yandex.ru>
2025-04-30 14:43:33 +0300

    Загружаю все изменения :)

--- Assets/Scripts/Save.cs
+++ Assets/Scripts/Save.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using TMPro;
 using Unity.VisualScripting;
 #if UNITY_EDITOR
@@ -18,12 +19,14 @@ using static UnityEngine.UI.Image;
 class GameData
 {
     public string type;
+    public string tag;
     public Vector3 position;
     public Quaternion rotation;
     public Vector3 scale;
     public bool active;
-    public GameData(string typ,Vector3 pos,Quaternion rot,Vector3 sca, bool act)
+    public GameData(string tagt, string typ,Vector3 pos,Quaternion rot,Vector3 sca, bool act)
     {
+        tag = tagt; 
         type = typ;
         position = pos;
         rotation = rot;
@@ -33,6 +36,10 @@ class GameData
     public string Type()
     {
         return type;
+    }
+    public string Tag()
+    {
+        return tag;
     }
     public Vector3 Position()
     {
@@ -51,7 +58,15 @@ class GameData
         return scale;
     }
 }
-
+public enum LevelObject
+{
+    polygon,
+    wall,
+    @static,
+    dynamic,
+    interactive,
+    hazard,
+}
 public static class JsonHelper
 {
     public static List<T> FromJson<T>(string json)
@@ -83,53 +98,76 @@ public static class JsonHelper
 #if UNITY_EDITOR
 [ExecuteInEditMode]
 #endif
-
 public class Save : MonoBehaviour
 {
-    private void LoadPrefab(string prefabName, Vector3 position, Quaternion rotation, Vector3 scale, bool active)
+
+    string[] tags = new string[5] { "Dynamic", "Static", "Interactive", "Player", "Road" };
+    List<List<List<LevelObject>>> levelObjects;
+    private void LoadPrefab(LevelObject prefabName, Vector3 position)
     {
-        var asset = Resources.Load<GameObject>(prefabName);
-        GameObject prefab = GameObject.Instantiate(asset);
+        var asset = Resources.Load<GameObject>($"{tag}\\{prefabName}");
+#if UNITY_EDITOR
+        GameObject prefab = (GameObject)PrefabUtility.InstantiatePrefab(asset);
+#else
+        GameObject prefab = (GameObject)GameObject.Instantiate(asset);
+#endif
         prefab.transform.position = new Vector3(position.x, position.y, position.z);
-        prefab.transform.rotation = rotation;
-        prefab.transform.localScale = scale;
-        prefab.SetActive(active);
-    }
+    } 
     private void LoadLevel()
     {
         string path = Path.GetFullPath($"{Application.persistentDataPath}\\Saves.json");
         StreamReader reader = new StreamReader(path);
         string levelJson = reader.ReadToEnd();
-        List<GameData> data = JsonHelper.FromJson<GameData>(File.ReadAllText(path));
-        foreach (GameData b in data)
+        var data = JsonHelper.FromJson<List<List<LevelObject>>>(File.ReadAllText(path));
+        for (int i = 0; i < data.Count; i++)
         {
-            try
+            for (int j = 0; j < data[i].Count; j++)
             {
-                LoadPrefab(b.Type(), b.Position(), b.Rotation(), b.Scale(), b.Active());
+                for(int k = 0; k < data[i][j].Count; k++)
+                {
+                    try
+                    {
+                        LoadPrefab(data[i][j][k], new Vector3(i,j,k));
+                    }
+                    catch { Debug.Log(data[i][j][k]); }
+                    
+                }
             }
-            catch { Debug.Log(b.Type()); }
         }
     }
-    public void SaveAll()
+    public void SaveAll(int Level2d, int Level3d, int Level1d)
     {
-        List<GameData> alldata = new List<GameData>();
-        foreach (GameObject t in SceneManager.GetActiveScene().GetRootGameObjects())
+        levelObjects = new(Level1d);
+        for (int i= 0;i<Level2d; i++)
         {
-            alldata.Add(new GameData(t.name, t.transform.position, t.transform.rotation, t.transform.localScale, t.activeSelf));
+            levelObjects[i] = new(Level2d);
+            for(int j= 0;j<Level3d;j++)
+            {
+                levelObjects[i][j] = new(j);
+            }
+        }
+        foreach (string tag in tags)
+        {
+            foreach (GameObject thing in GameObject.FindGameObjectsWithTag(tag))
+            {
+                levelObjects[(int)thing.transform.position.x][(int)thing.transform.position.y][(int)thing.transform.position.z] = LevelObject.interactive;
+            }
         }
         string json = "";
-        json = JsonHelper.ToJson<GameData>(alldata, true);
+
+        json = JsonHelper.ToJson<List<List<LevelObject>>>(levelObjects, true);
         File.WriteAllText(Application.persistentDataPath + "\\Saves.json", json);
         Debug.Log(json);
     }
     void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 150, 100), "Save"))
+    {                                        
+        if (GUI.Button(new Rect(10, 10, 150, 100), "Save"))                                                                                                                                                 
+
         {
-            SaveAll();
+            SaveAll(40,40,3);
         }
         if (GUI.Button(new Rect(10, 10 + 100, 150, 100), "Load"))
-        {
+        {       
             LoadLevel();
         }
     }
